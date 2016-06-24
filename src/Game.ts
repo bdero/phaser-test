33a41df5d6f23981955d31bfe7411165ebaf00f0
Brandon DeRosier <x@bdero.me>
2016-06-24 01:21:45 -0400

    Add initial project config

---
+++ src/Game.ts
@@ -0,0 +1,26 @@
+/// <reference path="../bin/js/phaser/typescript/phaser.d.ts" />
+
+class SimpleGame {
+    game:Phaser.Game;
+
+    constructor() {
+        this.game = new Phaser.Game(
+            800, 600, Phaser.AUTO, 'content',
+            { preload: this.preload, create: this.create }
+        );
+    }
+
+    preload() {
+        this.game.load.image('logo', 'assets/test.png');
+        this.game.stage.backgroundColor = 0xdeadbe;
+    }
+
+    create() {
+        var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
+        logo.anchor.setTo(0.5, 0.5);
+    }
+}
+
+window.onload = () => {
+    var game = new SimpleGame();
+}
